ebcb8122d3945004725b27607e25a4cad0ddc29d
frameyl <frameyl@163.com>
2014-01-04 08:22:25 -0800

    Add 3 unit test for ssmp dispatch

--- common/ssmp_dispatch.go
+++ common/ssmp_dispatch.go
@@ -8,6 +8,8 @@ import (
 type SsmpDispatch struct {
     // Name of dispatch
     name string
+    // Mode of Dispatch
+    mode int
     // Channel of packet buffer
     bufChan chan bytes.Reader
     // Channel of control message
@@ -15,7 +17,9 @@ type SsmpDispatch struct {
     // Channel of register
     regChan chan SsmpDispatchReg
     // Map from Magic Number to FSM input channel
-    mapInput map[uint64] chan bytes.Reader
+    mapFsm map[uint64] chan bytes.Reader
+    // Default input channel for unmapped packets
+    defaultFsm chan bytes.Reader
     // Counters
     DispatchCnt
 }
@@ -28,13 +32,20 @@ type SsmpDispatchReg struct {
 const SSMP_DISP_CLOSE       = 0
 const SSMP_DISP_RESET       = 1
 
-func NewSsmpDispatch(dispName string) *SsmpDispatch {
+const (
+    SSMP_DISP_SVR   = iota
+    SSMP_DISP_CLNT
+)
+
+func NewSsmpDispatch(dispName string, mode int) *SsmpDispatch {
     disp := &SsmpDispatch{
                 dispName,
+                mode,
                 make(chan bytes.Reader),
                 make(chan int),
                 make(chan SsmpDispatchReg),
                 make(map[uint64] chan bytes.Reader),
+                make(chan bytes.Reader),
                 DispatchCnt{} }
 
     return disp
@@ -78,33 +89,52 @@ func (disp *SsmpDispatch)Handle(nextStep chan bytes.Reader) (err error) {
         case packet := <-disp.bufChan:
             disp.Rx++
             // Packet length is less than the SSMP header
+            //fmt.Printf("debug1")
             if packet.Len() < LEN_SSMP_HDR {
                 // Not a valid SSMP packet, bypass it
-                nextStep <- packet
+                if nextStep != nil {
+                    nextStep <- packet
+                }
                 disp.Bypass++
+                //fmt.Printf("debug2")
+                continue
             }
 
             // Not a SSMP packet
+            //fmt.Printf("debug3")
             if isSsmpPkt, _ := IsSsmpPacket(&packet); !isSsmpPkt {
-                nextStep <- packet
+                if nextStep != nil {
+                    nextStep <- packet
+                }
                 disp.Bypass++
+                continue
             }
 
             // With a unknow message name
             if msgType, _ := ReadMsgType(&packet); msgType == MSG_UNKNOWN {
                 disp.Discard++
+                continue
             }
 
+            // Dispatch packet according to the magic number
             magic, _ := ReadMagicNum(&packet)
 
-            fsmChan, ok := disp.mapInput[magic]
+            fsmChan, ok := disp.mapFsm[magic]
             if ok {
                 fsmChan <- packet
                 disp.Handled++
+                continue
             } else {
-                // Discard if I'm a client
                 // Handle it as an incoming session if I'm a server
-                // FIXME
+                // Discard if I'm a client
+                if disp.mode == SSMP_DISP_CLNT {
+                    disp.Discard++
+                    continue
+                } else if disp.mode == SSMP_DISP_SVR {
+                    disp.defaultFsm <- packet
+                    disp.Handled++
+                    continue
+                }
             }
 
         case cmd := <-disp.cntlChan:
@@ -115,22 +145,23 @@ func (disp *SsmpDispatch)Handle(nextStep chan bytes.Reader) (err error) {
                 close(disp.regChan)
                 return nil
             } else if cmd == SSMP_DISP_RESET {
-                disp.mapInput = make(map[uint64] chan bytes.Reader)
+                disp.mapFsm = make(map[uint64] chan bytes.Reader)
             }
 
         case reg := <-disp.regChan:
             if reg.BufChan != nil {
-                if _, ok := disp.mapInput[reg.Magic]; ok {
-                    fmt.Printf("Try to register a MagicNum already existed %v", reg.Magic)
+                if _, ok := disp.mapFsm[reg.Magic]; ok {
+                    fmt.Printf("Try to register a MagicNum already existed %v\n", reg.Magic)
                     continue
                 }
-                disp.mapInput[reg.Magic] = reg.BufChan
+                disp.mapFsm[reg.Magic] = reg.BufChan
+                fmt.Printf("Register a MagicNum %X\n", reg.Magic)
             } else {
-                if _, ok := disp.mapInput[reg.Magic]; !ok {
-                    fmt.Printf("Try to unregister a MagicNum not existed %v", reg.Magic)
+                if _, ok := disp.mapFsm[reg.Magic]; !ok {
+                    fmt.Printf("Try to unregister a MagicNum not existed %v\n", reg.Magic)
                     continue
                 }
-                delete(disp.mapInput, reg.Magic)
+                delete(disp.mapFsm, reg.Magic)
             }
         }
     }
--- common/ssmp_dispatch_test.go
+++ common/ssmp_dispatch_test.go
@@ -1,14 +1,100 @@
 package common
 
 import "testing"
+import "time"
+import "fmt"
+import "bytes"
+
+/*
+var ssmp1 []byte=[]byte{'S', 'S', 'M', 'P', 'v', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        'H', 'e', 'l', 'l', 'o', 0, 0, 0,
+        0x11, 0x22, 0x33, 0x44, 0xaa, 0xbb, 0xcc, 0xdd,
+        'N', 'o', ' ', 'N', 'a', 'm', 'e', 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+var ssmpPkt1 *bytes.Reader = bytes.NewReader(ssmp1)
+
+var ssmp2 []byte=[]byte{'S', 'S', 'M', 'P', 'v', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        'R', 'e', 'p', 'l', 'y', 0, 0, 0,
+        0, 0, 0x1a, 0, 0x2b, 0, 0x3c, 0,
+        'S', 'l', 'i', 'e', 'n', 't', ' ', 'L', 'a', 'm', 'p', 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0x1a, 0x2b, 0x3c, 0x4d }
+var ssmpPkt2 *bytes.Reader = bytes.NewReader(ssmp2)
+
+var ssmpE1 []byte=[]byte{'S', 'S', 'M', 'P', 'v', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        'r', 'e', 'p', 'l', 'y', '!', 0, 0,
+        'm', 'a', 'g', '3', 'M', 'A', 'G', '4',
+        'S', 'l', 'i', 'e', 'n', 't', ' ', 'L', 'a', 'm', 'p', 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0x1a, 0x2b, 0x3c, 0x4d }
+var ssmpEPkt1 *bytes.Reader = bytes.NewReader(ssmpE1)
+
+var sample1 []byte=[]byte{'S', 'S', 'M', 'P', 'V', '1'}
+var samplePkt1 *bytes.Reader = bytes.NewReader(sample1)
+
+var sample2 []byte=[]byte{'S', 'S', 'M', 'P', 'V', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+var samplePkt2 *bytes.Reader = bytes.NewReader(sample2)
+*/
 
 func TestSsmpDispatch(t *testing.T) {
     var dispMng DispatchMng
-    ssmpDisp := NewSsmpDispatch("Ssmp Connector")
+    ssmpDisp := NewSsmpDispatch("Ssmp Connector", SSMP_DISP_CLNT)
 
     dispMng.Add(ssmpDisp)
     dispMng.Start()
 
+    dispMng.Handle(*samplePkt1)
+    
+    timer1 := time.NewTicker(time.Second)
+    for i := 0; i < 10; i++ {
+        select {
+        case <- timer1.C:
+            fmt.Printf("Bypass %v", ssmpDisp.Bypass)
+            if ssmpDisp.Bypass == 1 {
+                goto EOW1
+            }
+        }
+    }
+EOW1:    
+    if ssmpDisp.Bypass < 1 {
+        t.Errorf("SSMP Dispatch failed to bypass a non-SSMP packet, RX %v, Bypass %v", ssmpDisp.Rx, ssmpDisp.Bypass)
+    }
+    
+    dispMng.Handle(*ssmpPkt1)
+
+    for i := 0; i < 10; i++ {
+        select {
+        case <- timer1.C:
+            fmt.Printf("Discard %v", ssmpDisp.Discard)
+            if ssmpDisp.Discard == 1 {
+                goto EOW2
+            }
+        }
+    }
+EOW2:
+    if ssmpDisp.Discard < 1 {
+        t.Errorf("SSMP Dispatch failed to bypass a non-SSMP packet, RX %v, Bypass %v", ssmpDisp.Rx, ssmpDisp.Bypass)
+    }
+    
+    chanInput := make(chan bytes.Reader)
+    ssmpDisp.Register(0x11223344aabbccdd, chanInput)
+    
+    dispMng.Handle(*ssmpPkt1)
+    var buf bytes.Reader
+
+    for i := 0; i < 10; i++ {
+        select {
+        case <- timer1.C:
+            fmt.Printf("Handeld %v", ssmpDisp.Handled)
+        case buf = <- chanInput:
+            goto EOW3
+        }
+    }
+EOW3:
+    fmt.Printf("Packet Length %v", buf.Len())
+    if buf.Len() == 0 {
+        t.Errorf("SSMP Dispatch: didn't get the packet")
+    }
 }
 
 
